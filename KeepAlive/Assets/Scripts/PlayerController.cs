c8046b81e7c603db27280d0e9327852754941215
isienmai <ben.strutt@hotmail.co.uk>
2020-04-18 14:31:31 +0100

    Change movement to directly modify object position. Created Player prefab with camera.

--- KeepAlive/Assets/Scripts/PlayerController.cs
+++ KeepAlive/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   public float speed;
+
   private Rigidbody2D playerRigidbody;
+
+  private Vector2 currentVelocity;
 
   void Start()
   {
     playerRigidbody = GetComponent<Rigidbody2D>();
+    currentVelocity = new Vector2(0, 0);
   }
 
   void FixedUpdate()
@@ -17,7 +21,8 @@ public class PlayerController : MonoBehaviour
     float moveHorizontal = Input.GetAxis("Horizontal");
     float moveVertical = Input.GetAxis("Vertical");
 
-    Vector2 movementForce = new Vector2(moveHorizontal, moveVertical);
-    playerRigidbody.AddForce(movementForce * speed);
+    Vector2 movementForce = new Vector2(moveHorizontal, moveVertical) * speed;    
+
+    playerRigidbody.MovePosition(playerRigidbody.position + movementForce * Time.deltaTime);
   }
 }
