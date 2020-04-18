ce65001de2545de45aeb238781703c28280fad41
isienmai <ben.strutt@hotmail.co.uk>
2020-04-18 14:06:00 +0100

    Add basic additive movement prototype.

---
+++ KeepAlive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed;
+  private Rigidbody2D playerRigidbody;
+
+  void Start()
+  {
+    playerRigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector2 movementForce = new Vector2(moveHorizontal, moveVertical);
+    playerRigidbody.AddForce(movementForce * speed);
+  }
+}
