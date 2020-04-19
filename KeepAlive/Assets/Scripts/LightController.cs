20e46d91b7ce59cdb16b966890dac1c8ba9f7aa5
Jacob Green <faux113g@gmail.com>
2020-04-19 17:22:00 +0100

    Added light control script
    change curves on the script to adjust the light

---
+++ KeepAlive/Assets/Scripts/LightController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    public AnimationCurve LightIntensityCurve;
+    public AnimationCurve OuterRadiusCurve;
+    public AnimationCurve InnerRadiusCurve;
+
+    [SerializeField]
+    private Light2D light2DComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Torch torch = FindObjectOfType<Torch>();
+        if (torch)
+        {
+            torch.onTorchHealthUpdated += UpdateLight;
+        }
+    }
+
+    // Update is called once per frame
+    void UpdateLight(float health)
+    {
+        float healthPercentage = health / 100.0f;
+        ApplyLightValues(healthPercentage);
+    }
+
+    void ApplyLightValues(float percentage)
+    {
+        if (light2DComponent)
+        {
+            light2DComponent.pointLightOuterRadius = OuterRadiusCurve.Evaluate(percentage);
+            light2DComponent.pointLightInnerRadius = InnerRadiusCurve.Evaluate(percentage);
+            light2DComponent.intensity = LightIntensityCurve.Evaluate(percentage);
+        }
+    }
+}
