56b740699fb29353ff84aadceaead0ab949f9dbb
Jordan Rowe <jordanneilrowe@live.co.uk>
2020-04-19 20:14:06 +0100

    light flicker

--- KeepAlive/Assets/Scripts/LightController.cs
+++ KeepAlive/Assets/Scripts/LightController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
 public class LightController : MonoBehaviour
@@ -8,34 +6,43 @@ public class LightController : MonoBehaviour
     public AnimationCurve LightIntensityCurve;
     public AnimationCurve OuterRadiusCurve;
     public AnimationCurve InnerRadiusCurve;
+    public AnimationCurve FlickerCurve;
 
-    [SerializeField]
-    private Light2D light2DComponent;
+    [SerializeField] private Light2D light2DComponent;
+    [SerializeField] private float flickerLife = 2.0f; // seconds
 
-    // Start is called before the first frame update
-    void Start()
+    private float lightHealthPercentage = 1.0f;
+    private float flickerTimer = 0.0f;
+
+    private void Start()
     {
         Torch torch = FindObjectOfType<Torch>();
-        if (torch)
+
+        if ( torch )
         {
             torch.onTorchHealthUpdated += UpdateLight;
         }
     }
 
-    // Update is called once per frame
-    void UpdateLight(float health)
+    private void UpdateLight( float health )
     {
-        float healthPercentage = health / 100.0f;
-        ApplyLightValues(healthPercentage);
+        lightHealthPercentage = health / 100.0f;
     }
 
-    void ApplyLightValues(float percentage)
+    private void Update()
     {
-        if (light2DComponent)
+        flickerTimer += Time.deltaTime;
+
+        if ( flickerTimer > flickerLife )
         {
-            light2DComponent.pointLightOuterRadius = OuterRadiusCurve.Evaluate(percentage);
-            light2DComponent.pointLightInnerRadius = InnerRadiusCurve.Evaluate(percentage);
-            light2DComponent.intensity = LightIntensityCurve.Evaluate(percentage);
+            flickerTimer = 0.0f;
+        }
+
+        if ( light2DComponent )
+        {
+            light2DComponent.pointLightOuterRadius = OuterRadiusCurve.Evaluate( lightHealthPercentage );
+            light2DComponent.pointLightInnerRadius = InnerRadiusCurve.Evaluate( lightHealthPercentage );
+            light2DComponent.intensity = LightIntensityCurve.Evaluate( lightHealthPercentage ) * FlickerCurve.Evaluate( flickerTimer / flickerLife );
         }
     }
 }
