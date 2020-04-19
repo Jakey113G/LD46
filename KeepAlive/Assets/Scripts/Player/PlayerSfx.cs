78c17bcb11f1c71f884429d91fc9dd34a71c895e
Jordan Rowe <jordanneilrowe@live.co.uk>
2020-04-19 17:44:31 +0100

    game music and player footsteps

---
+++ KeepAlive/Assets/Scripts/Player/PlayerSfx.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerSfx : MonoBehaviour
+{
+    [SerializeField] private float stepsPerSecond = 4;
+    [SerializeField] private AudioClip[] footSteps;
+    [SerializeField] private AudioSource audioSource;
+
+    private float stepsFrequency => 1.0f / stepsPerSecond;
+
+    private float timer = 0.0f;
+    private int footStepsIndex = 0;
+
+    private Rigidbody2D playerRigidbody;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        if ( playerRigidbody.velocity.magnitude <= 0.2f )
+        {
+            footStepsIndex = 0;
+            timer = stepsFrequency;
+            
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        if ( timer >= stepsFrequency )
+        {
+            timer = 0.0f;
+            footStepsIndex++;
+
+            if ( footStepsIndex >= footSteps.Length )
+            {
+                footStepsIndex = 0;
+            }
+
+            audioSource.PlayOneShot( footSteps[footStepsIndex] );
+        }
+    }
+}
